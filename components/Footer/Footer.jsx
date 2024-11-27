b78d9e62f80bcf6add88778ad963c62589707be5
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-27 16:14:29 +0100

    ADD box shadow and background colors

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import style from './Footer.module.css'
+
 export default function Footer() {
     return (
         <footer>
-            <div className="container">
-                Footer
+            <div className={style.container}>
+                <div className={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam asperiores inventore vel enim rem repellendus tenetur dicta, repudiandae quae distinctio dignissimos porro culpa deleniti aut.</div>
             </div>
         </footer>
     )
