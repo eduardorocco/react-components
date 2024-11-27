78ab4c603b609ac89996fb8b30675692b08a8ecb
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-27 15:16:45 +0100

    ADD card img and lorem

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -2,10 +2,10 @@ export default function Card() {
     return (
         <div className="card">
             <div className="body">
-                <div className="image">immagine</div>
-                <div className="title">titolo</div>
-                <div className="text">testo</div>
-                <div className="button">bottone</div>
+                <div className="image"><img src="https://picsum.photos/600/400" alt="" /></div>
+                <div className="title">Titolo del post</div>
+                <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat iure provident consectetur expedita natus quia doloribus asperiores beatae maxime reiciendis repellat numquam voluptate iusto corrupti quidem nulla placeat ut dicta et quo quod, rerum magni ab ipsum? Magnam delectus explicabo sunt! Consectetur hic debitis quo corrupti aliquam doloremque iste illo?</div>
+                <div className="button">Leggi di più</div>
             </div>
         </div>
     )
